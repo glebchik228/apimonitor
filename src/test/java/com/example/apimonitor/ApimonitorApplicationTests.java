6955b975558d974e9a5e94c08b7e9c3368488216
glebchik_gg <gaben123001@gmail.com>
2025-04-18 12:13:43 +0300

    first commit

---
+++ src/test/java/com/example/apimonitor/ApimonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apimonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApimonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
